dfc6f2e3fc791c516c02836d0bccb5281a84d6fc
Matthew Parris <mfrogparris@gmail.com>
2019-12-25 21:07:42 -0600

    Add js folder passthrough copy

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ "public/img": "img" });
     eleventyConfig.addPassthroughCopy({ "public/css": "css" });
+    eleventyConfig.addPassthroughCopy({ "public/js": "js" });
 
     return {
         templateFormats: [
