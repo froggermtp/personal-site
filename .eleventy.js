5fba1a06aff174a4e64192df66cfb84f45bf4073
Matthew Parris <mfrogparris@gmail.com>
2019-12-31 00:20:09 -0600

    Add filter for readable dates

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ "public/css": "css" });
     eleventyConfig.addPassthroughCopy({ "public/js": "js" });
 
+    eleventyConfig.addFilter('dateReadable', function (date) {
+        return date.toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' });
+    });
+
     return {
         templateFormats: [
             "md",
