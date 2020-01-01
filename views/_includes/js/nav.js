3e2b42a4d0f8cec6995d3fa68c94ba54c9ccd808
Matthew Parris <mfrogparris@gmail.com>
2020-01-01 03:12:11 -0600

    Make the mobile menu more accessible
    - Can now tab to hamburger icon & invoke the mobile menu with enter key

--- views/_includes/js/nav.js
+++ views/_includes/js/nav.js
@@ -4,3 +4,10 @@ menuButton.addEventListener('click', function (e) {
     document.querySelector('nav').classList.toggle('fullscreen');
     menuButton.classList.toggle('change');
 });
+
+menuButton.addEventListener("keyup", function (event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        menuButton.click();
+    }
+}); 
