21e8ff6bb91d49db6744842199b69cd6f40dc255
Matthew Parris <mfrogparris@gmail.com>
2020-10-18 16:59:32 -0500

    Add wrapped image shortcode

--- utils/pairedShortcodes.js
+++ utils/pairedShortcodes.js
@@ -1,13 +1,27 @@
 const outdent = require('outdent');
 
 module.exports = {
-    image: function (content, href) {
+    image: function (content, href, options = {}) {
+        const classes = options.classes && options.classes.join(' ');
+
         return outdent`
         <img
-            class="img"
+            class="img ${classes}"
             src="/assets/images${href}"
             alt="${content}"
         />
+        `;
+    },
+
+    wrappedImage: function (content, href, options = {}) {
+        const optionsClasses = options.classes ? options.classes : [];
+        const classes = ['wrapper__image', ...optionsClasses];
+
+        return outdent`
+        <figure class="wrapper">
+            ${module.exports.image(content, href, { classes })}
+            ${options.useCaption ? `<figcaption>${content}</figcaption>` : ''}
+        </figure>
         `;
     }
 }
