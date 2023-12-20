4baac2d6ce8b5d7d60b1ba0a7877e90abaf15de2
Matthew Parris <mfrogparris@gmail.com>
2023-12-20 23:10:58 +0000

    Add GitHub Actions info to build.txt

--- src/data/build.js
+++ src/data/build.js
@@ -1,4 +1,10 @@
 module.exports = {
     env: process.env.ELEVENTY_ENV,
-    timestamp: new Date()
+    timestamp: new Date(),
+    gitRefName: process.env.GITHUB_REF_NAME,
+    gitSha: process.env.GITHUB_SHA,
+    workflowRunID: process.env.GITHUB_RUN_ID,
+    workflowRunAttempt: process.env.GITHUB_RUN_ATTEMPT,
+    workflowRunNumber: process.env.GITHUB_RUN_NUMBER,
+    workflowURL: `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`
 }
